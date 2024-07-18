afddba0ffa858f43ee75e64a8bcbdc4e210bcf92
Mihir <anuj.nits@gmail.com>
2024-07-18 20:45:08 +0530

    eighth commit

---
+++ day_59/main.py
@@ -0,0 +1,31 @@
+from datetime import datetime as dt
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+all_posts = requests.get('https://api.npoint.io/674f5423f73deab1e9a7').json()
+today = dt.now().strftime('%Y-%m-%d')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', posts=all_posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:num>')
+def post(num):
+    return render_template('post.html', num=num - 1, posts=all_posts, author='Anuj Gupta', date=today)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
