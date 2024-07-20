56dbdb30c8903015bd414db44b60b3f360c20d54
Mihir <anuj.nits@gmail.com>
2024-07-20 10:58:46 +0530

    eighth commit

---
+++ day_61/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+from wtf_forms import MyForm
+
+app = Flask(__name__)
+app.secret_key = 'a random string'
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    login_form = MyForm()
+    login_form.validate_on_submit()
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
