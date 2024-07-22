f44946c9ec9dd25c59118784dcee4084a91e9aa4
anuj_gupta1 <anuj.gupta1@spglobal.com>
2024-07-22 09:44:53 +0530

    eighth commit

--- day_61/main.py
+++ day_61/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
 from wtf_forms import MyForm
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 app.secret_key = 'a random string'
 
 
@@ -14,9 +16,13 @@ def home():
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     login_form = MyForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == 'admin@email.com' and login_form.password.data == '12345678':
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, port=8080)
--- day_61/wtf_forms.py
+++ day_61/wtf_forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import InputRequired, Email, Length
 
 
 class MyForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired(), Email()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[InputRequired(), Email()])
+    password = PasswordField(label='Password', validators=[InputRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
